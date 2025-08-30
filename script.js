b4071a9ba10e2d7a90de8339643e9f8b2fd67298
kamauwebworksdev <kamau.webworks@gmail.com>
2025-08-30 11:27:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+    const navLinks = document.getElementById("navLinks");
+
+    function showMenu(){
+        navLinks.style.right = "0";
+    }
+
+     function hideMenu(){
+        navLinks.style.right ="-200px";
+
+     }
+
+    
+     
+
+
+
+     /* <script>
+
+    //JavaScript for Toggle Menu
+
+    //Get the menu and buttons
+
+    const navLinks = document.getElementById("navLinks");
+    const openMenu = document.querySelector(".fa-bars");
+    const closeMenu = document.querySelector(".fa-times");
+
+    //open menu on hamburger click
+    openMenu.addEventListener("click", function(){
+    navLinks.style.right = "0";
+});
+
+//close Menu on X icon click
+closeMenu.addEventListener('click', function(){
+    navLinks.style.right = "-200px";
+});
+
+</script>
+
+<script>
+    //Method number 2
+
+    /* 
+    var navLinks = document.getElementById("navLinks");
+    function showMenu(){
+        navLinks.style.right = "0";
+    }
+
+    function hideMenu(){
+        navLinks.style.right = "-200px";
+    }
+</script>
+
+
+
+ */
